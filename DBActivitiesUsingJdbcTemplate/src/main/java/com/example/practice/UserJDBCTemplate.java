8912ae6fb1d98d6ae3579de4bc46be464982ee17
shiva-gandluri <46939066+shiva-gandluri@users.noreply.github.com>
2020-05-25 11:15:42 +0530

    Update UserJDBCTemplate.java

--- DBActivitiesUsingJdbcTemplate/src/main/java/com/example/practice/UserJDBCTemplate.java
+++ DBActivitiesUsingJdbcTemplate/src/main/java/com/example/practice/UserJDBCTemplate.java
@@ -1,23 +1,24 @@
 package com.example.practice;
 
 import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserJDBCTemplate implements UserDAO{
-	   private DataSource dataSource;
-	   private JdbcTemplate jdbcTemplate;
-	@Override
-	public void setDataSource(DataSource dataSource) {
-		// TODO Auto-generated method stub
-	      this.dataSource = dataSource;
-	      this.jdbcTemplate = new JdbcTemplate(this.dataSource);
-	}
+	   //private DataSource dataSource;
+	   //private JdbcTemplate jdbcTemplate;
+	   @Qualifier("mysqlService")
+		@Autowired
+		JdbcTemplate mysqlService;
+
 	@Override
 	public void update(Integer id, String First, String Last, Integer age) {
 		// TODO Auto-generated method stub
 		String Update_EMPLOYEE_SQL="INSERT INTO Registration " +
 				"VALUES ("+id+", '"+First+"', '"+Last+"', "+age+")";
-		jdbcTemplate.update(Update_EMPLOYEE_SQL, id, First, Last, age);
+		mysqlService.update(Update_EMPLOYEE_SQL, id, First, Last, age);
 		return ;
 		
 	}
@@ -25,9 +26,14 @@ public class UserJDBCTemplate implements UserDAO{
 	public UserDetails getUserDetails() {
 		// TODO Auto-generated method stub
 	      String SQL = "select * from employee";
-	      UserDetails employee = jdbcTemplate.queryForObject(
+	      UserDetails employee = mysqlService.queryForObject(
 	         SQL, new UserDetailsRowMapper());
 	      return employee;
+	}
+	@Override
+	public void setDataSource(DataSource ds) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
